afdd5cb10e3d009e8e4dec1dea973040dfa254b2
Henrique-camargo <henriquecnobrega0711@gmail.com>
2025-12-27 22:01:27 -0300

    Projeto iniciado

---
+++ src/main/java/com/lifestream/api/LifeStreamApplication.java
@@ -0,0 +1,13 @@
+package com.lifestream.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LifeStreamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LifeStreamApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lifestream/api/LifeStreamApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lifestream.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LifeStreamApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
